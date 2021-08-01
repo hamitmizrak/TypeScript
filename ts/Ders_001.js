1448fc0b47abd9aa2a04573c94cb8a28b297b3f9
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 18:41:54 +0300

    typescript nedir,var,let,

---
+++ ts/Ders_001.js
@@ -0,0 +1,15 @@
+// let adi:string="Hamit";
+// let dizi:string[]=["44" ,"Battalgazi","YeşilYurt","Hamit","Mızrak"];
+// console.log(...dizi);
+// let sayi:number=44;
+// let sayi44:number=55;
+//Hoisting ts izin vermez
+//DataTypes
+// var , let , const
+//TS: number,string,boolean ,any:joker
+var sayi = 44.55; //virgüllü sayılar
+var sayi2 = 66; //tam sayılar
+var SABIT = 3.14555;
+var isim = "Hamit";
+var sonuc;
+sonuc = true;
---
+++ ts/Ders_001.ts
@@ -0,0 +1,19 @@
+// let adi:string="Hamit";
+
+// let dizi:string[]=["44" ,"Battalgazi","YeşilYurt","Hamit","Mızrak"];
+// console.log(...dizi);
+
+// let sayi:number=44;
+// let sayi44:number=55;
+
+//Hoisting ts izin vermez
+
+//DataTypes
+// var , let , const
+//TS: number,string,boolean ,any:joker
+var sayi: number = 44.55; //virgüllü sayılar
+var sayi2: number = 66; //tam sayılar
+const SABIT:number=3.14555;
+let isim: string = "Hamit";
+let sonuc: boolean;
+sonuc = true;
