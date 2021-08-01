129eae86121d50ca3b249d215229430a9f0da71f
HamitMizrak <hamitmizrak@gmail.com>
2021-08-01 19:15:56 +0300

    oop ==> enum

---
+++ ts/Ders_002_oop_enum.ts
@@ -0,0 +1,13 @@
+// oop //
+// 1-) ENUM = sabit ifadeler
+//Dünyada her yerde aynı olan birimler için kullanıyoruz.
+
+//small , medium ,large ,xlarge
+//pazartesi,salı,çarşamba,perşembe,cuma,cumartesi,pazar
+
+
+enum Olculer{
+    small , medium ,large ,xlarge
+}
+
+let olcuDegeri=Olculer.large;
